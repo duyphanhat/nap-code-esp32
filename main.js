43f426ba02c062ce95e224a4cc752435993ab7aa
duyphanhat <nhatduycodientu@gmail.com>
2026-06-08 04:38:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const path = require('path');
 
-// Bat Web Serial API tren file:// protocol
 app.commandLine.appendSwitch('enable-experimental-web-platform-features');
 app.commandLine.appendSwitch('enable-features', 'WebSerial');
 app.commandLine.appendSwitch('enable-blink-features', 'WebSerial');
-// Cho phep Web Serial hoat dong tren file:// (quan trong nhat)
-app.commandLine.appendSwitch('unsafely-treat-insecure-origin-as-secure', 'file://');
-app.commandLine.appendSwitch('allow-running-insecure-content');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -17,35 +13,35 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      webSecurity: false, // Tat web security de Serial + module hoat dong tren file://
+      webSecurity: false,
+      preload: path.join(__dirname, 'preload.js'),
     },
-    frame: true,
-    backgroundColor: '#d4d0c8',
-    show: false,
+    frame: true, backgroundColor: '#d4d0c8', show: false,
   });
 
   Menu.setApplicationMenu(null);
-
   win.webContents.session.setPermissionCheckHandler(() => true);
   win.webContents.session.setDevicePermissionHandler(() => true);
 
+  let pendingCallback = null;
+
   win.webContents.session.on('select-serial-port', (event, portList, webContents, callback) => {
     event.preventDefault();
-    if (portList.length > 0) callback(portList[0].portId);
-    else callback('');
+    pendingCallback = callback;
+    // Gui danh sach port co ten that len trang
+    win.webContents.send('port-list', portList);
   });
 
-  // Load file truc tiep - type=module hoat dong tot
-  win.loadFile('index.html');
+  ipcMain.on('port-chosen', (event, portId) => {
+    if (pendingCallback) { pendingCallback(portId); pendingCallback = null; }
+  });
 
+  win.loadFile('index.html');
   win.once('ready-to-show', () => { win.show(); win.focus(); });
 }
 
 app.whenReady().then(() => {
   createWindow();
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  });
+  app.on('activate', () => { if (BrowserWindow.getAllWindows().length === 0) createWindow(); });
 });
-
 app.on('window-all-closed', () => { app.quit(); });
